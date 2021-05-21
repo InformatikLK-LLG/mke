7c49a9e6aa05517ace2f4d86df191faaea16242e
Hannah Franken <hfranken@hfranken.de>
2021-05-21 17:39:18 +0200

    Add Routes to App. Implement Wrapper to wrap around content, so that adding headers, footers, etc. is easier

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,32 @@
-import './App.css';
-import ProvideAuth from "./hooks/useAuth"
+import ProvideAuth from "./hooks/useAuth";
+import PrivateRoute from "./components/PrivateRoute";
+import Wrapper from "./Wrapper";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ProvideAuth>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="" element={<Wrapper />}>
+              <PrivateRoute path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              {/* 
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+            <Route path="/register" element={<Register />}>
+              <Route path="/" element={<Register0 />} />
+              <Route path="1" element={<Register1 />} />
+              <Route path="2" element={<Register2 />} />
+              NEIN
+            </Route> */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ProvideAuth>
   );
 }
 
