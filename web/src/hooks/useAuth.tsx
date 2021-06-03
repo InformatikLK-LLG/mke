367e342fbaddae0fa2d897a43a7a44625d6fb53b
Hannah Franken <hfranken@hfranken.de>
2021-06-03 22:38:12 +0200

    Connect server-side login process with uiuiui

--- web/src/hooks/useAuth.tsx
+++ web/src/hooks/useAuth.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-type user = { email: string; password: string } | undefined;
+import axios from "axios";
 
 type register = (
   code: number,
@@ -10,6 +10,16 @@ type register = (
   password: string,
   passwordRepeated: string
 ) => void;
+
+type user =
+  | {
+      firstName: string;
+      lastName: string;
+      email: string;
+      username: string;
+      password: string;
+    }
+  | undefined;
 
 type auth = {
   user: user;
@@ -49,9 +59,14 @@ export const useAuth = () => {
 function useProvideAuth(): auth {
   const [user, setUser] = useState<user>();
 
-  const signin = (email: string, password: string) => {
+  const signin = async (email: string, password: string) => {
     // TODO implement logic for signin
-    setUser({ email, password });
+    const { data } = await axios.post("http://localhost:8080/login", {
+      email,
+      password,
+    });
+    console.log(data);
+    setUser(data as user);
   };
 
   const signout = () => {
