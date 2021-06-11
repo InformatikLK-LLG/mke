6ee13610dc6c3be10526a49b89aef7db7edd207d
Mathis Kuhl <mathis.kuhl@llg-giessen.de>
2021-06-11 12:54:04 +0200

    CustomerController created

--- server/src/main/java/de/llggiessen/mke/controller/CustomerController.java
+++ server/src/main/java/de/llggiessen/mke/controller/CustomerController.java
@@ -3,9 +3,20 @@ package de.llggiessen.mke.controller;
 import de.llggiessen.mke.repository.CustomerRepository;
 import de.llggiessen.mke.schema.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(path = "/customer")
 public class CustomerController {
 
     @Autowired
     CustomerRepository repository;
+
+    @GetMapping("")
+    public Iterable<Customer> getCustomers() {
+        return repository.findAll();
+    }
 }
