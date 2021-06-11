1005cf46aadd9194436cc39369d232252b51daca
Marius Damm <mariusdamm1010@gmail.com>
2021-06-11 12:32:43 +0200

    add bookingRepository

--- server/src/main/java/de/llggiessen/mke/repository/bookingRepository.java
+++ server/src/main/java/de/llggiessen/mke/repository/bookingRepository.java
@@ -1,11 +1,7 @@
 package de.llggiessen.mke.repository;
 
 import de.llggiessen.mke.schema.Booking;
-import de.llggiessen.mke.schema.User;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
