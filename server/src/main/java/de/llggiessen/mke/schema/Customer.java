ae23bbff2136efc74bd24ec70c1dbb4a000d63b0
jannik <Jann>
2021-06-11 12:17:51 +0200

    created customer 1/2

--- server/src/main/java/de/llggiessen/mke/schema/Customer.java
+++ server/src/main/java/de/llggiessen/mke/schema/Customer.java
@@ -1,4 +1,29 @@
 package de.llggiessen.mke.schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class Customer {HI
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long customerId;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String mobilePhone;
+    private String businessPhone;
+
 }
