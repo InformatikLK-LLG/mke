5ec1076cdae6cf2804ee0d9bc790dd19e634a0e5
Daniel <daniel.dietzler@web.de>
2021-06-17 18:36:33 +0200

    Generate flat accessKeys array and add pagination. Update npm packages.
    Co-authored-by: Hannah Franken <hfrank3n@users.noreply.github.com>

--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -364,8 +364,6 @@ export default function Table<T extends SimplestItem>({
     setPage(0);
   };
 
-  stableSort(rows, getComparator(direction, sortBy));
-
   return (
     <>
       <TableContainer className={classes.tableContainer}>
