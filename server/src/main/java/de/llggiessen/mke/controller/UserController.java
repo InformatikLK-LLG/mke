1a3c02483426e6520c211959e5292c71ba1211f1
jannik <Jann>
2021-06-30 08:10:39 +0200

    Fix files unintentionally added

--- server/src/main/java/de/llggiessen/mke/controller/UserController.java
+++ server/src/main/java/de/llggiessen/mke/controller/UserController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @RequestMapping(path = "/user")
 public class UserController {
+
     @Autowired
     UserRepository repository;
 
