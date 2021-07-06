2ca28b5cc22b5cb43e657272ee2230a410d39232
rtschv <82653802+rtschv@users.noreply.github.com>
2021-07-06 16:29:09 +0200

    Create booking endpoint (#18)
    * add booking schema
    
    * add bookingRepository
    
    * add bookingRepository
    
    * change name from bookingRepository to BookingRepository
    
    * add BookingController
    
    * fix embedded attributes
    
    * change names in Booking schema
    
    * fix error: duplicated column in booking schema
    
    * change inner class from static to non-static in booking schema
    
    * add ability to filter by attributes
    
    * fix convert error
    
    * optimize imports
    
    * add query for id, status, date
    
    * fix filter for date
    
    * add query for checking whether entity is in between dates
    
    Co-authored-by: Marius <45565475+UZMSora@users.noreply.github.com>
    Co-authored-by: Marius Damm <mariusdamm1010@gmail.com>
    Co-authored-by: Elias Bennour <esieben1310@gmail.com>

---
+++ server/src/main/java/de/llggiessen/mke/repository/BookingRepository.java
@@ -0,0 +1,32 @@
+package de.llggiessen.mke.repository;
+
+import de.llggiessen.mke.schema.Booking;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+@RepositoryRestResource(exported = false)
+public interface BookingRepository extends CrudRepository<Booking, Long> {
+
+    @Query(value = "SELECT * FROM booking WHERE booking.year LIKE %:year%", nativeQuery = true)
+    Iterable<Booking> findAllByYear(@Param("year") String year);
+
+    @Query(value = "SELECT * FROM booking WHERE booking.id LIKE %:id%", nativeQuery = true)
+    Optional<Booking> findByID(@Param("id") long id);
+
+    @Query(value = "SELECT * FROM booking WHERE booking.retrieval_date LIKE %:retrievalDate%", nativeQuery = true)
+    Iterable<Booking> findAllByRetrievalBoatDate(@Param("retrievalDate") String retrievalDate);
+
+    @Query(value = "SELECT * FROM booking WHERE booking.return_date LIKE %:returnDate%", nativeQuery = true)
+    Iterable<Booking> findAllByReturnBoatDate(@Param("returnDate") String returnDate);
+
+    @Query(value = "SELECT * FROM booking WHERE booking.status LIKE %:status%", nativeQuery = true)
+    Iterable<Booking> findAllByStatus(@Param("status") char status);
+
+    @Query(value = "SELECT * FROM booking WHERE booking.retrieval_date BETWEEN :retrievalDate AND :returnDate AND booking.return_date BETWEEN :retrievalDate AND :returnDate", nativeQuery = true)
+    Iterable<Booking> findAllInRange(@Param("retrievalDate") String retrievalDate, @Param("returnDate") String returnDate);
+}
---
+++ server/src/main/java/de/llggiessen/mke/schema/Booking.java
@@ -0,0 +1,48 @@
+package de.llggiessen.mke.schema;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Time;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private int numberOfParticipants;
+    @Embedded
+    private RetrievalBoat retrievalBoat;
+    @Embedded
+    private ReturnBoat returnBoat;
+    private String year;
+    private double spaceUtilizationFee;
+    private char status;
+    private int salePercent;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Embeddable
+    public static class RetrievalBoat {
+        private Date retrievalDate;
+        private Time retrievalTime;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Embeddable
+    public static class ReturnBoat {
+        private Date returnDate;
+        private Time returnTime;
+    }
+}
