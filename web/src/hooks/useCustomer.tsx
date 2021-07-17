7dbe4bdd3cc0fe6c0538d8d5b729925af6e8f4ba
hfrank3n <hfranken@hfranken.de>
2021-07-17 16:32:30 +0200

    Add useCustomer hook for getting customers by their id
    Co-authored-by: Daniel Dietzler <danieldietzler@users.noreply.github.com>

---
+++ web/src/hooks/useCustomer.tsx
@@ -0,0 +1,13 @@
+import { CustomerType } from "../pages/Customer";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const useCustomer = (id: string) => {
+  return useQuery(["customer", id], () =>
+    axios.get<CustomerType>("http://localhost:8080/customer", {
+      params: { id },
+    })
+  );
+};
+
+export default useCustomer;
