aae7fa60805c41d1f8f0da68503fa4721e9055fc
hfrank3n <hfranken@hfranken.de>
2021-08-29 18:49:00 +0200

    Catch case when there is no error response given. Change color of error messages by imagination. i did not change anything.
    Co-authored-by: Daniel Dietzler <danieldietzler@users.noreply.github.com>

--- web/src/Wrapper.tsx
+++ web/src/Wrapper.tsx
@@ -81,7 +81,7 @@ export default function Wrapper() {
     (response) => response,
     async (error) => {
       const originalRequest = error.config;
-      if (error.response.status === 401 && !originalRequest._retry) {
+      if (error.response?.status === 401 && !originalRequest._retry) {
         originalRequest._retry = true;
         await axios.post("http://localhost:8080/profile/refreshToken");
         return axios(originalRequest);
