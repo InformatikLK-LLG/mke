4fa10310235304ce9e02acda1c15a16bdadbd2e1
Daniel <daniel.dietzler@web.de>
2021-09-11 18:24:26 +0200

    Add ability to filter to useRoles
    Co-authored-by: Hannah Franken <hfrank3n@users.noreply.github.com>

--- web/src/hooks/useRoles.tsx
+++ web/src/hooks/useRoles.tsx
@@ -1,15 +1,24 @@
 import { RoleType } from "./useAuth";
 import axios from "axios";
 import { useQuery } from "react-query";
+import { useState } from "react";
+
+export type RoleSearchParams = { id?: string };
 
 const useRoles = () => {
+  const [searchParams, setSearchParams] = useState<
+    RoleSearchParams | undefined
+  >();
   return {
-    ...useQuery(["roles"], async () => {
+    ...useQuery(["roles", searchParams], async () => {
       const { data } = await axios.get<Array<RoleType>>(
-        "http://localhost:8080/role"
+        "http://localhost:8080/role",
+        { params: searchParams }
       );
       return data;
     }),
+    searchParams,
+    setSearchParams,
   };
 };
 
