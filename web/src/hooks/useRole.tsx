6953700e914cf7584aa01d3db71a63935633c4a1
Daniel <daniel.dietzler@web.de>
2021-09-11 18:53:30 +0200

    Fetch specific role and cache pre-fetched roles
    Co-authored-by: Hannah Franken <hfrank3n@users.noreply.github.com>

---
+++ web/src/hooks/useRole.tsx
@@ -0,0 +1,14 @@
+import { RoleType } from "./useAuth";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const useRole = (id: string) => {
+  return useQuery(["role", id], async () => {
+    const { data } = await axios.get<RoleType>(
+      `http://localhost:8080/role/${id}`
+    );
+    return data;
+  });
+};
+
+export default useRole;
--- web/src/hooks/useRoles.tsx
+++ web/src/hooks/useRoles.tsx
@@ -1,22 +1,33 @@
+import { QueryClient, useQuery, useQueryClient } from "react-query";
+
 import { RoleType } from "./useAuth";
 import axios from "axios";
-import { useQuery } from "react-query";
 import { useState } from "react";
 
 export type RoleSearchParams = { id?: string };
 
 const useRoles = () => {
+  const queryClient = useQueryClient();
   const [searchParams, setSearchParams] = useState<
     RoleSearchParams | undefined
   >();
   return {
-    ...useQuery(["roles", searchParams], async () => {
-      const { data } = await axios.get<Array<RoleType>>(
-        "http://localhost:8080/role",
-        { params: searchParams }
-      );
-      return data;
-    }),
+    ...useQuery(
+      ["roles", searchParams],
+      async () => {
+        const { data } = await axios.get<Array<RoleType>>(
+          "http://localhost:8080/role",
+          { params: searchParams }
+        );
+        return data;
+      },
+      {
+        onSuccess: (roles) =>
+          roles.forEach((role) =>
+            queryClient.setQueryData(["role", role.id], role)
+          ),
+      }
+    ),
     searchParams,
     setSearchParams,
   };
